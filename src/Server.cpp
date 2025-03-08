46e61b3c6ba6359b52e711ea34f271d25a3ae23f
Manish Singh <singhmanish.personal@gmail.com>
2025-03-08 15:18:51 +0530

    general

--- src/Server.cpp
+++ src/Server.cpp
@@ -8,37 +8,40 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <thread>
-
+#include <vector>
 
 using namespace std;
 
+vector<string> processRESPCommand(string &buffer);
 
+void handleRequest(int clientSocket)
+{
 
-void handleRequest(int clientSocket) {
-  
   string readBuffer;
 
-  while(true){
-  char buffer[1024];
+  while (true)
+  {
+    char buffer[1024];
 
-  memset(buffer,0,sizeof(buffer));
+    memset(buffer, 0, sizeof(buffer));
 
-  int recv_bytes=recv(clientSocket, buffer, 1024, 0);
- 
-    if(recv_bytes==0){
+    int recv_bytes = recv(clientSocket, buffer, 1024, 0);
+
+    if (recv_bytes == 0)
+    {
       std::cout << "Client disconnected\n";
       close(clientSocket);
       return;
     }
-    readBuffer.append(buffer,recv_bytes);
-    cout<<"Read buffer size: "<<readBuffer.size()<<endl;
-    cout<<"Read buffer: "<<readBuffer<<endl;
-  char *message="+PONG\r\n";
-  send(clientSocket, message, strlen(message), 0);
-}
+    readBuffer.append(buffer, recv_bytes);
+    cout<<processRESPCommand(readBuffer)<<endl;
+    char *message = "+PONG\r\n";
+    send(clientSocket, message, strlen(message), 0);
+  }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
@@ -46,63 +49,129 @@ int main(int argc, char **argv) {
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
-  //test1();
+  // test1();
 
   // Uncomment this block to pass the first stage
-  
-   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-   if (server_fd < 0) {
+
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0)
+  {
     std::cerr << "Failed to create server socket\n";
     return 1;
-   }
-  
-   // Since the tester restarts your program quite often, setting SO_REUSEADDR
-   // ensures that we don't run into 'Address already in use' errors
-   int reuse = 1;
-   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-     std::cerr << "setsockopt failed\n";
-     return 1;
-   }
-  //
-   struct sockaddr_in server_addr;
-   server_addr.sin_family = AF_INET;
-   server_addr.sin_addr.s_addr = INADDR_ANY;
-   server_addr.sin_port = htons(6379);
-  //
-   if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-     std::cerr << "Failed to bind to port 6379\n";
-     return 1;
-   }
-  //
-   int connection_backlog = 5;
-   if (listen(server_fd, connection_backlog) != 0) {
-     std::cerr << "listen failed\n";
-     return 1;
-   }
-  
-
-   struct sockaddr_in client_addr;
-   int client_addr_len = sizeof(client_addr);
-  
-   std::cout << "Waiting for a client to connect...\n";
-
-
-   while(true){
-    
-
-   
-
-  int clientSocket = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  std::cout << "Client connected\n";
-    thread t(handleRequest, clientSocket);
-    
-    t.detach();
-
   }
-  
-   close(server_fd);
+
+  // Since the tester restarts your program quite often, setting SO_REUSEADDR
+  // ensures that we don't run into 'Address already in use' errors
+  int reuse = 1;
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
+  {
+    std::cerr << "setsockopt failed\n";
+    return 1;
+  }
+  //
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(6379);
+  //
+  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
+  {
+    std::cerr << "Failed to bind to port 6379\n";
+    return 1;
+  }
+  //
+  int connection_backlog = 5;
+  if (listen(server_fd, connection_backlog) != 0)
+  {
+    std::cerr << "listen failed\n";
+    return 1;
+  }
+
+  struct sockaddr_in client_addr;
+  int client_addr_len = sizeof(client_addr);
+
+  std::cout << "Waiting for a client to connect...\n";
+
+  while (true)
+  {
+
+    int clientSocket = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
+    std::cout << "Client connected\n";
+    thread t(handleRequest, clientSocket);
+
+    t.detach();
+  }
+
+  close(server_fd);
 
   return 0;
 }
 
+vector<string> processRESPCommand(string &buffer)
+{
 
+  int position = 0;
+  vector<string> command;
+
+  int sizeOfbuffer = buffer.size();
+
+  if (sizeOfbuffer < 1)
+  {
+    return "NVC";
+  }
+
+  if (buffer[position] != '*')
+  {
+    return "NVC";
+  }
+
+  position++;
+  int commandLength = buffer[position];
+
+  position++;
+
+  if (buffer[position] != '\r' && buffer[position + 1] != '\n')
+  {
+    return "NVC";
+  }
+
+  position += 2;
+
+  for (int i = 0; i < commandLength; i++)
+  {
+
+    if (buffer[position] != '$')
+    {
+      return "NVC";
+    }
+
+    position++;
+    int tokenLength = buffer[position];
+
+    position++;
+
+    if (buffer[position] != '\r' && buffer[position + 1] != '\n')
+    {
+      return "NVC";
+    }
+
+    position += 2;
+
+    for (int j = 0; j < tokenLength; j++)
+    {
+      command.push_back(buffer[position]);
+      position++;
+    }
+
+    if (buffer[position] != '\r' && buffer[position + 1] != '\n')
+    {
+      return "NVC";
+    }
+
+    position += 2;
+
+
+  }
+  retun command;
+ 
+}
