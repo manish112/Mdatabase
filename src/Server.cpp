1f7445983b9c915066c17d1bb9b908fd4fbdd5ec
Manish Singh <singhmanish.personal@gmail.com>
2025-03-09 11:16:53 +0530

    mdatabase

--- src/Server.cpp
+++ src/Server.cpp
@@ -280,7 +280,7 @@ string processArray(vector<string> &command, unordered_map<string, string> &memo
     transform(subCommand.begin(), subCommand.end(), subCommand.begin(), ::toupper);
     if (subCommand=="GET") {
       if (configs.find(command[2])!=configs.end()) {
-        return "*2\r\n$"+to_string(command[2].size())+"\r\n"+command[2]+"\r\n$"+to_string(configs[command[1]].size())+"\r\n"+configs[command[1]]+"\r\n";
+        return "*2\r\n$"+to_string(command[2].size())+"\r\n"+command[2]+"\r\n$"+to_string(configs[command[2]].size())+"\r\n"+configs[command[2]]+"\r\n";
       }
       else {
         return "$-1\r\n";
